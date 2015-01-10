99acaf023aa3ee0868e307eb972a00859f47a703
Andrew Gentry <andrew.gentry@colorado.edu>
2015-01-10 13:52:28 -0800

    Implemented google maps and added the Objective C Header Bridge.

--- Caravan/ViewController.swift
+++ Caravan/ViewController.swift
@@ -14,6 +14,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var camera = GMSCameraPosition.cameraWithLatitude(-33.86,
+            longitude: 151.20, zoom: 6)
+        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        mapView.myLocationEnabled = true
+        self.view = mapView
+        
+        var marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        
+        mapView.mapType = kGMSTypeSatellite
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Cbridge.m
@@ -0,0 +1,9 @@
+//
+//  Cbridge.m
+//  Caravan
+//
+//  Created by Andrew Gentry on 1/10/15.
+//  Copyright (c) 2015 Caravan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
