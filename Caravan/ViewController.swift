742d74d3bccef06e3ee67111ecf3b0e4e4651a57
mguida22 <michael.guida@colorado.edu>
2015-01-11 01:16:21 -0800

    Adding support for tracking your phone or free panning

--- Caravan/ViewController.swift
+++ Caravan/ViewController.swift
@@ -2,7 +2,6 @@
 //  ViewController.swift
 //  Caravan
 //
-//
 //  Created by Ben Williams on 1/10/15.
 //  Copyright (c) 2015 Caravan. All rights reserved.
 //
@@ -16,12 +15,26 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     let spanY = 0.015
     let spanX = 0.015
     
+    var isTracking = true
+    
     var annotationList: [MKPointAnnotation] = []
     
+    
     @IBOutlet weak var theMap: MKMapView!
+    @IBOutlet weak var trackingSwitch: UISwitch!
     
     var manager:CLLocationManager!
     var myLocations: [CLLocation] = []
+    
+    @IBAction func doTracking(sender: AnyObject) {
+        if trackingSwitch.on {
+            //follow user
+            isTracking = true
+        } else {
+            //stop following
+            isTracking = false
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,7 +50,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         theMap.delegate = self
         theMap.mapType = MKMapType.Standard
         theMap.showsUserLocation = true
-        
     }
     
     func locationManager(manager:CLLocationManager, didUpdateLocations locations:[AnyObject]) {
@@ -55,16 +67,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         
         annotationList.append(annotation)
         
-        var newRegion = MKCoordinateRegion(center: theMap.userLocation.coordinate, span: MKCoordinateSpanMake(spanX, spanY))
-        theMap.setRegion(newRegion, animated: true)
+        if isTracking {
+            var newRegion = MKCoordinateRegion(center: theMap.userLocation.coordinate, span: MKCoordinateSpanMake(spanX, spanY))
+            theMap.setRegion(newRegion, animated: true)
+        }
+        
     }
 }
-
-
-
-
-
-
-
-
-
