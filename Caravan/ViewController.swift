cc18ad0da76b590ae8fb83b1b8aaf9f164686980
Andrew Gentry <andrew.gentry@colorado.edu>
2015-01-11 00:17:07 -0800

    An attempt at the navigations

--- Caravan/ViewController.swift
+++ Caravan/ViewController.swift
@@ -15,10 +15,17 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     
     let spanY = 0.015
     let spanX = 0.015
+    var myRoute : MKRoute?
+    
+    
     
     var annotationList: [MKPointAnnotation] = []
     
     @IBOutlet weak var theMap: MKMapView!
+    @IBAction func unwindToViewController (sender: UIStoryboardSegue){
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
     
     var manager:CLLocationManager!
     var myLocations: [CLLocation] = []
@@ -26,6 +33,50 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        // Set up the points for the directions request.
+        var point1 = MKPointAnnotation()
+        var point2 = MKPointAnnotation()
+        
+        point1.coordinate = CLLocationCoordinate2DMake(25.0305, 121.5360)
+        point1.title = "Taipei"
+        point1.subtitle = "Taiwan"
+        theMap.addAnnotation(point1)
+        
+        
+        point2.coordinate = CLLocationCoordinate2DMake(24.9511, 121.2358)
+        point2.title = "Chungli"
+        point2.subtitle = "Taiwan"
+        theMap.addAnnotation(point2)
+        
+        theMap.centerCoordinate = point2.coordinate
+        theMap.delegate = self
+        
+        var directionsRequest = MKDirectionsRequest()
+        let markTaipei = MKPlacemark(coordinate: CLLocationCoordinate2DMake(point1.coordinate.latitude, point1.coordinate.longitude), addressDictionary: nil)
+        let markChungli = MKPlacemark(coordinate: CLLocationCoordinate2DMake(point2.coordinate.latitude, point2.coordinate.longitude), addressDictionary: nil)
+        
+        directionsRequest.setSource(MKMapItem(placemark: markChungli))
+        directionsRequest.setDestination(MKMapItem(placemark: markTaipei))
+        directionsRequest.transportType = MKDirectionsTransportType.Automobile
+        
+        var directions = MKDirections(request: directionsRequest)
+        
+        directions.calculateDirectionsWithCompletionHandler{(response: MKDirectionsResponse!, error:NSError!) -> Void in
+            if error == nil {
+                self.myRoute = response.routes[0] as? MKRoute
+                self.theMap.addOverlay(self.myRoute?.polyline)
+            }
+        }
+        
+        func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+            
+            var myLineRenderer = MKPolylineRenderer(polyline: myRoute?.polyline!)
+            myLineRenderer.strokeColor = UIColor.redColor()
+            myLineRenderer.lineWidth = 3
+            return myLineRenderer
+        }
+    
         //Setup our Location Manager
         manager = CLLocationManager()
         manager.delegate = self
---
+++ SettingsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SettingsViewController.swift
+//  Caravan
+//
+//  Created by Andrew Gentry on 1/10/15.
+//  Copyright (c) 2015 Caravan. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
